82f7650856fd8e8678ec4ab0845fb0fa2977ef5b
Aku76 <steeven.cauchois@gmail.com>
2015-12-29 23:46:43 +0100

    feature(Game) : create refresh funciton for squares

--- IA/public/javascript/awale.js
+++ IA/public/javascript/awale.js
@@ -1,14 +1,23 @@
 //alert($("h1").text());
 //$("#awale-board").createElement('div'));
+/*
+    Initialise la partie.
+    Stock a 0 et cases à 4
+*/
 function InitGame(){
     $('.square').each(function(){
-        $(this).attr('data-pawn-number',4)
-        $(this).text($(this).data('pawn-number'));
+        $(this).attr('data-pawn-number',4);
+        refresh($(this));
     });
     $('.stock').each(function(){
-        $(this).attr('data-pawn-number',0)
-        $(this).text($(this).data('pawn-number'));
+        $(this).attr('data-pawn-number',0);
+        refresh($(this));
     });
+}
+
+//actualise une case selon son data-pawn-number
+function refresh(x){
+    x.text(x.data('pawn-number'));
 }
 
 $(function(){
