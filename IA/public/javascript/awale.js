7778ac4c1530b2a3b8371f6112db5c90bbfb99b5
Aku76 <steeven.cauchois@gmail.com>
2015-12-29 23:12:05 +0100

    feature(Game) : Init with data done

--- IA/public/javascript/awale.js
+++ IA/public/javascript/awale.js
@@ -1,2 +1,16 @@
 //alert($("h1").text());
 //$("#awale-board").createElement('div'));
+function InitGame(){
+    $('.square').each(function(){
+        $(this).attr('data-pawn-number',4)
+        $(this).text($(this).data('pawn-number'));
+    });
+    $('.stock').each(function(){
+        $(this).attr('data-pawn-number',0)
+        $(this).text($(this).data('pawn-number'));
+    });
+}
+
+$(function(){
+    $("[awale-init]")[0].onclick=InitGame;
+});
