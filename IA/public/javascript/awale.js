e2712713fa27b1f0b12d5c6d80fa312516e567b9
Aku76 <steeven.cauchois@gmail.com>
2016-01-03 19:15:47 +0100

    feature(play) : play(n) now distribute point correctly

--- IA/public/javascript/awale.js
+++ IA/public/javascript/awale.js
@@ -14,18 +14,21 @@ function InitGame(){
         $(this).attr('data-pawn-number',0);
         refresh($(this));
     });
+    TopPlayerTurn = false;
+    console.log("    bottom-player to move")
 }
 /*
 function Coucou(x) {
 	alert ("Hello! : " + x);
 }
 */
-
+var TopPlayerTurn = false;
 //actualise une case selon son data-pawn-number
 function refresh(x){
     x.text(x.data('pawn-number'));
 }
 
+//actualise toutes les cases ayant un data-pawn-number
 function refreshAll(){
     $('[data-pawn-number').each(function(){
         refresh($(this));
@@ -34,9 +37,11 @@ function refreshAll(){
 
 //joue la case numero X
 function play(position){
+    //recuperation des données de la case jouée
     var clickedDiv = $('[data-position=' + position + ']');
     var pawnNumber = clickedDiv.data('pawn-number');
     clickedDiv.data('pawn-number', 0);
+    //distribution des jetons
     while(pawnNumber > 0){
         position = (position + 1) % 12;
         var div = $('[data-position=' + position + ']');
@@ -44,8 +49,48 @@ function play(position){
         div.data('pawn-number', divPawnNumber + 1);
         pawnNumber--;
     }
+
+
+    var divPawnNumber = div.data('pawn-number');
+
+    if (TopPlayerTurn){
+        console.log("top-player move : ")
+    } else {
+        console.log("bottom-player move : ")
+    }
+    console.log("  end-position : " + position);
+    console.log("  divPawnNumber : " + divPawnNumber);
+
+
+
+    //ramassage des pions si bon coté && 3 ou 2 pions
+    while(((position > 5 && TopPlayerTurn) || (position < 5 && !TopPlayerTurn)) && (divPawnNumber == 2 || divPawnNumber == 3)){
+        if (TopPlayerTurn){
+            var stock = $('#stock-top');
+        } else {
+            var stock = $('#stock-bottom');
+        }
+        console.log(stock);
+        var stockPawnNumber = stock.data('pawn-number');
+        console.log( stockPawnNumber + "+" + divPawnNumber);
+        stock.data('pawn-number', stockPawnNumber + divPawnNumber);
+        div.data('pawn-number', 0);
+
+        //mise a jour des variables
+        position = (position - 1);
+        var div = $('[data-position=' + position + ']');
+        var divPawnNumber = div.data('pawn-number');
+        
+    }
     refreshAll();
-    return pawnNumber;
+    TopPlayerTurn = !TopPlayerTurn;
+
+    if (TopPlayerTurn){
+        console.log("    top-player to move")
+    } else {
+        console.log("    bottom-player to move")
+    }
+    return TopPlayerTurn;
 }
 
 $(function(){
