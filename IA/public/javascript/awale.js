7a9e88f772b3b0bb9e8b3c3a6654a22868c3dc22
Affluxx <affluxxx@gmail.com>
2016-01-19 00:08:32 +0100

    feature<gameboard> : Add the gameboard part. Move every method in it. Not working yet

--- IA/public/javascript/awale.js
+++ IA/public/javascript/awale.js
@@ -2,7 +2,7 @@
     Initialise la partie.
     Stock a 0 et cases à 4
 */
-
+/*
 function InitGame(){
     $('.square').each(function(){
         $(this).attr('data-pawn-number',4);
@@ -15,14 +15,25 @@ function InitGame(){
     });
     TopPlayerTurn = false;
 }
+*/
 // Fonction qui affiche les règles du jeu.
 function rules() {
     //alert("Règles du jeu Awale : Ici a ajouter.");
     window.location.href = "rules.html"
 }
-// Variable utile pour le jeu.
-var TopPlayerTurn = false;
-var win = false;
+
+$(function(){
+    // Initialise le gameboard
+    var gameboard = new Gameboard();
+    //Initialise le jeu
+    $("[awale-init]")[0].onclick=gameboard.InitGame();
+    // Affiche les règles.
+    $("[awale-rules]")[0].onclick=rules;
+    // Ajouter la partie qui recupere la position a laquelle
+    // Le gameboard dois jouer (le data-position)
+    //$("")
+});
+/*
 //actualise une case selon son data-pawn-number
 function refresh(x){
     x.text(x.data('pawn-number'));
@@ -34,8 +45,9 @@ function refreshAll(){
         refresh($(this));
     });
 }
-
+*/
 // Verifie la condition de victoire : Plus de 25 pions
+/*
 function isWin(){
     $('.stock').each(function(){
         if ($(this).data('pawn-number') > 24){
@@ -48,63 +60,76 @@ function isWin(){
         }
     });
 }
+*/
+/*
+// Variable utile pour le jeu.
+var TopPlayerTurn = false;
+var win = false;
+
 
 //joue la case numero position
-function play(position){
-    if(((position <= 5 && TopPlayerTurn) || (position > 5 && !TopPlayerTurn)) && !win){
+/*
+La fonction Play joue a la case choisis par l'argument position.
+Cette fonction est placé dans le gameboard puisque c'est sur lui que le jeu se fait.
+*/
+/*
+    function play(position){
+        if(((position <= 5 && TopPlayerTurn) || (position > 5 && !TopPlayerTurn)) && !win){
 
-        //recuperation des données de la case jouée
-        var clickedDiv = $('[data-position=' + position + ']');
-        var pawnNumber = clickedDiv.data('pawn-number');
-        if (pawnNumber > 0) {
-            clickedDiv.data('pawn-number', 0);
-            //distribution des jetons
-            var oldPosition = position
-            while(pawnNumber > 0) {
-                position = (position + 1) % 12;
-                // On ne repose pas de pion a l'ancienne position donc on skip
-                if (oldPosition != position) {
+            //recuperation des données de la case jouée
+            var clickedDiv = $('[data-position=' + position + ']');
+            var pawnNumber = clickedDiv.data('pawn-number');
+            if (pawnNumber > 0) {
+                clickedDiv.data('pawn-number', 0);
+                //distribution des jetons
+                var oldPosition = position
+                while(pawnNumber > 0) {
+                    position = (position + 1) % 12;
+                    // On ne repose pas de pion a l'ancienne position donc on skip
+                    if (oldPosition != position) {
+                        var div = $('[data-position=' + position + ']');
+                        var divPawnNumber = div.data('pawn-number');
+                        div.data('pawn-number', divPawnNumber + 1);
+                        pawnNumber--;
+                    }
+                }
+                //mise a jour du pion
+                var divPawnNumber = div.data('pawn-number');
+                //ramassage des pions si bon coté && 3 ou 2 pions
+                while(((position > 5 && TopPlayerTurn) || (position <= 5 && !TopPlayerTurn)) && (divPawnNumber == 2 || divPawnNumber == 3)){
+                    if (TopPlayerTurn){
+                        var stock = $('#stock-top');
+                    } else {
+                        var stock = $('#stock-bottom');
+                    }
+
+                    var stockPawnNumber = stock.data('pawn-number');
+
+                    stock.data('pawn-number', stockPawnNumber + divPawnNumber);
+                    div.data('pawn-number', 0);
+
+                    //mise a jour des variables
+                    position = (position - 1);
                     var div = $('[data-position=' + position + ']');
                     var divPawnNumber = div.data('pawn-number');
-                    div.data('pawn-number', divPawnNumber + 1);
-                    pawnNumber--;
+                    
                 }
+                refreshAll();
+                isWin();
+                TopPlayerTurn = !TopPlayerTurn;
             }
-            //mise a jour du pion
-            var divPawnNumber = div.data('pawn-number');
-            //ramassage des pions si bon coté && 3 ou 2 pions
-            while(((position > 5 && TopPlayerTurn) || (position <= 5 && !TopPlayerTurn)) && (divPawnNumber == 2 || divPawnNumber == 3)){
-                if (TopPlayerTurn){
-                    var stock = $('#stock-top');
-                } else {
-                    var stock = $('#stock-bottom');
-                }
-
-                var stockPawnNumber = stock.data('pawn-number');
-
-                stock.data('pawn-number', stockPawnNumber + divPawnNumber);
-                div.data('pawn-number', 0);
-
-                //mise a jour des variables
-                position = (position - 1);
-                var div = $('[data-position=' + position + ']');
-                var divPawnNumber = div.data('pawn-number');
-                
-            }
-            refreshAll();
-            isWin();
-            TopPlayerTurn = !TopPlayerTurn;
         }
     }
-}
+    */
+
+
+
 //Fonction qui affiche la dernière case atteinte par la case choisit.
+/*
 function watch(position) {
 }
+*/
 
-$(function(){
-    $("[awale-init]")[0].onclick=InitGame;
-    $("[awale-rules]")[0].onclick=rules;
-});
 
 
 
